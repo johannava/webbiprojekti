c40216a92cb96d04e91a70f2aba00b2103262583
Johanna Varanka <johanna.varanka@gmail.com>
2017-09-19 19:41:02 +0300

    Created javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+function ShowAlert() {
+    console.log("terve!");
+    alert("Hei!");
+}
+function showText(text) {
+    let elem = document.getElementById("teksti-kappale");
+    elem.textContent = text;
+}
