9ec78f20915b3c6e0c4025563ada856fad37c336
Johanna Varanka <johanna.varanka@gmail.com>
2017-09-26 19:33:29 +0300

    Added toggleClass function and some pictures

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function ShowAlert() {
     console.log("terve!");
     alert("Hei!");
+
 }
 function showText(text) {
     let elem = document.getElementById("teksti-kappale");
     elem.textContent = text;
 }
+
+function toggleClass(elemId, className){
+    let elem = document.getElementById(elemId);
+    elem.classList.toggle(className);
+}
+
+
+
